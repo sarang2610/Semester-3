c91c21633ec0c86bd07319330e55565dba51f627
sarang2610 <sarnagladva@gmail.com>
2022-09-25 16:00:51 +0530

    .

---
+++ data structure/programms/lab-3/EmployeeDemo.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Employee_Detail{
+	int Employee_id;
+	String Name;
+	String Designation;
+	double Salary;
+
+	Scanner sc = new Scanner(System.in);
+
+	void readDetails(){
+		System.out.println("Enter Employee_id : ");
+		int Employee_id = sc.nextInt();
+		System.out.println("Enter Name : ");
+		String Name = sc.next();
+		System.out.println("Enter Designation : ");
+		String Designation = sc.next();
+		System.out.println("Enter Salary : ");
+		double Salary = sc.nextDouble();
+		System.out.println(Employee_id);
+		System.out.println(Name);
+		System.out.println(Designation);
+		System.out.println(Salary);
+	}
+}
+
+
+public class EmployeeDemo{
+	public static void main(String[] args) {
+		Employee_Detail e1 = new Employee_Detail();
+		e1.readDetails();
+	}
+}
---
+++ data structure/programms/lab-3/classDemo.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+class Student_Detail{
+	long enrollment_no;
+	String name;
+	int sem;
+	double cpi;
+	public Student_Detail(long enrollment_no,String name,int sem,double cpi){
+		this.enrollment_no=enrollment_no;
+		this.name=name;
+		this.sem=sem;
+		this.cpi=cpi;
+	}
+	public void printDetails(){
+		System.out.println("Enrollment No. : "+enrollment_no);
+		System.out.println("Name : "+name);
+		System.out.println("Semester : "+sem);
+		System.out.println("CPI : "+cpi);
+	}
+}
+public class classDemo{
+	public static void main(String[] args) {
+		Student_Detail obj[]=new Student_Detail[5];
+		obj[0]=new Student_Detail(1106,"yash",3,10);
+		obj[1]=new Student_Detail(1105,"krish",4,9);
+		obj[2]=new Student_Detail(1104,"deep",5,8);
+		obj[3]=new Student_Detail(1103,"jadu",6,7.90);
+		obj[4]=new Student_Detail(1102,"mangu",7,9.9);
+		for(int i=0;i<5;i++){
+			System.out.println("-----Details of student "+(i+1)+"-----");
+			obj[i].printDetails();
+		}
+	}
+}
